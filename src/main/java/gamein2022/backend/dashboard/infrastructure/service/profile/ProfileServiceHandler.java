e5cb78d7095bd11527eef788ec01cd43b22d913c
AliLordLoss <a.a.ghanati@gmail.com>
2023-05-16 13:30:53 +0330

    -fix: removed unused repo

--- src/main/java/gamein2022/backend/dashboard/infrastructure/service/profile/ProfileServiceHandler.java
+++ src/main/java/gamein2022/backend/dashboard/infrastructure/service/profile/ProfileServiceHandler.java
@@ -1,18 +1,11 @@
 package gamein2022.backend.dashboard.infrastructure.service.profile;
 
-import gamein2022.backend.dashboard.core.exception.BadRequestException;
-import gamein2022.backend.dashboard.core.exception.UserNotFoundException;
 import gamein2022.backend.dashboard.core.sharedkernel.entity.News;
-import gamein2022.backend.dashboard.core.sharedkernel.entity.User;
 import gamein2022.backend.dashboard.core.sharedkernel.enums.NewsType;
 import gamein2022.backend.dashboard.infrastructure.repository.NewsRepository;
-import gamein2022.backend.dashboard.infrastructure.repository.UserRepository;
-import gamein2022.backend.dashboard.web.dto.request.ProfileInfoRequestDTO;
 import gamein2022.backend.dashboard.web.dto.result.NewsNotifsDTO;
-import gamein2022.backend.dashboard.web.dto.result.ProfileInfoResultDTO;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 
